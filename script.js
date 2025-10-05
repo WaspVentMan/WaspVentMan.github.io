bbf8dd53373d7d4a50c5f1d814df082a2667328d
WaspVentMan <66962696+WaspVentMan@users.noreply.github.com>
2025-10-05 14:49:48 +0100

    404 fix & baby steps map

--- script.js
+++ script.js
@@ -35,9 +35,9 @@ setInterval (()=>{
 
     let scrollTop = document.querySelector(".peaks87container").getBoundingClientRect().bottom
 
-    document.querySelector(".peaks87towerRender").style.bottom = Math.round(((scrollTop/8) - 256)%1024)-1024 + "px"
-    document.querySelector(".peaks87logo").style.bottom = Math.round((scrollTop/4)) + "px"
+    document.querySelector(".peaks87towerRender").style.bottom = (((scrollTop/8) - 256)%1024)-1024 + "px"
+    document.querySelector(".peaks87logo").style.bottom = ((scrollTop/4)) + "px"
     for (let x = 0; x < 14; x++){
-        document.querySelector(".peaks87paralax"+x).style.bottom = Math.round(((scrollTop-512)/(32/(x+1)))-256)+192+[0, 30][(x==13)+0] + "px"
+        document.querySelector(".peaks87paralax"+x).style.bottom = (((scrollTop-512)/(32/(x+1)))-256)+192+[0, 30][(x==13)+0] + "px"
     }
-}, 1000/60)
+}, 1000/144)
